ec6b41ac9d55eeab4eac71954c665951097b162f
OctopusLian <zoctopus@qq.com>
2020-06-30 22:30:34 +0800

    Site updated: 2020-06-30 22:30:32

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 27 2020 15:37:53 GMT+0800 (CST)
+// build time:Tue Jun 30 2020 22:29:46 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
