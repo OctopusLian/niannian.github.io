dcfc3061a99ca2ac2557a4aa6c6debe09ce1763d
OctopusLian <zoctopus@qq.com>
2021-02-06 21:16:12 +0800

    Site updated: 2021-02-06 21:15:11

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 02 2021 14:55:00 GMT+0800 (China Standard Time)
+// build time:Sat Feb 06 2021 21:13:12 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
