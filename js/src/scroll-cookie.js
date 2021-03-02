25d3cacba824c526f20fa97234a080ba10b7dc5e
root <root@LAPTOP-G68G84AC.localdomain>
2021-03-03 02:13:21 +0800

    Site updated: 2021-03-03 02:10:07

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 02 2021 22:20:33 GMT+0800 (China Standard Time)
+// build time:Wed Mar 03 2021 01:59:43 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
